1409037c96881b81f9c6ffb5a83bdbde5ec0f18a
Max Hallgarten <max.hallgarten@gmail.com>
2021-02-16 21:44:35 +0000

    Update method calls

--- assam/main.py
+++ assam/main.py
@@ -6,12 +6,12 @@ from gmatInterface import gmatInterface
 from visibilityModule import visibilityModule
 
 # Set parameters
-start_time = Time("2021-01-23 12:30")
-end_time = Time("2021-01-24 12:30")
+start_time = Time("2021-03-20 12:00")
+end_time = Time("2021-03-27 12:00")
 keplerian_elements = {"SMA": 7000,
                       "ECC": 0,
                       "INC": 98.6,
-                      "RAAN": 0,
+                      "RAAN": 90,
                       "AOP": 0,
                       "TA": 0}
 
@@ -22,7 +22,7 @@ gmat.execute_script()
 gmat.load_state()
 
 # Calculate target visibility
-visibility = visibilityModule(gmat.satellite_state, gmat.satellite_frame)
+visibility = visibilityModule(gmat.satellite_frame)
 visibility.get_solar_bodies()
 visibility.get_targets()
-visibility.calculate_separation()
+visibility.calculate_visibility()
