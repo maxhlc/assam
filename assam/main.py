4934e4bebe0ee22680bc25e89cd39f569e7fd346
Max Hallgarten <max.hallgarten@gmail.com>
2021-05-28 10:59:12 +0100

    Updated license and added to file headers

--- assam/main.py
+++ assam/main.py
@@ -1,5 +1,29 @@
 #!/usr/bin/env python
 
+"""
+MIT License
+
+Copyright (c) 2020-2021 Max Hallgarten La Casta
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
 from astropy.time import Time, TimeDelta
 from astropy import units as u
 import inspect
@@ -13,14 +37,12 @@ from scheduling_module import SchedulingModule
 # (allows viewing through the variable explorer)
 local_vars = {}
 
-# TODO: copyright notices
-
 plot_bitmaps = False
 
 def main():
     # Set parameters
     start_time = Time("2021-03-20 12:00")
-    end_time = Time("2021-03-20 12:10")
+    end_time = Time("2021-04-20 12:00")
     time_step = TimeDelta(5*u.min)
     keplerian_elements = {"SMA": 7000,
                           "ECC": 0,
